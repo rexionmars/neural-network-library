9d567da4f395f7b6ca8ba254a586806ce2e713b1
rexionmars <opensource.leonardi@gmail.com>
2023-05-24 23:51:06 -0300

    Improved output log

--- xor.c
+++ xor.c
@@ -17,10 +17,7 @@ typedef struct {
     float and_bias;
 } Xor;
 
-/*
-* Sigmoid function
-* S(x) = 1 / 1 + e**-x = e**x / e**x + 1 = 1 - S(-x)
-*/
+//S(x) = 1 / 1 + e**-x = e**x / e**x + 1 = 1 - S(-x)
 float sigmoidf(float x)
 {
     return 1.f / (1.f + expf(-x));
@@ -34,6 +31,7 @@ float forward(Xor model, float x1, float x2)
 }
 
 typedef float sample[3];
+
 sample xor_train[] = {
     {0, 0, 0},
     {1, 0, 1},
@@ -41,7 +39,36 @@ sample xor_train[] = {
     {1, 1, 0},
 };
 
-sample *train = xor_train;
+sample or_train[] = {
+    {0, 0, 0},
+    {1, 0, 1},
+    {0, 1, 1},
+    {1, 1, 1},
+};
+
+sample and_train[] = {
+    {0, 0, 0},
+    {1, 0, 0},
+    {0, 1, 0},
+    {1, 1, 1},
+};
+
+sample nand_train[] = {
+    {0, 0, 1},
+    {1, 0, 1},
+    {0, 1, 1},
+    {1, 1, 0},
+};
+
+sample nor_train[] = {
+    {0, 0, 1},
+    {1, 0, 0},
+    {0, 1, 0},
+    {1, 1, 0},
+};
+
+
+sample *train = nor_train;
 size_t train_count = 4;
 
 float cost(Xor model)
@@ -112,66 +139,100 @@ Xor finite_difference(Xor model, float eps)
 {
     Xor gate;
     float c = cost(model);
-    float saved;
+    float stored_value;
 
-    saved = model.or_weight1;
+    stored_value = model.or_weight1;
     model.or_weight1 += eps;
     gate.or_weight1 = (cost(model) - c) / eps;
-    model.or_weight1 = saved;
+    model.or_weight1 = stored_value;
 
-    saved = model.or_weight2;
+    stored_value = model.or_weight2;
     model.or_weight2 += eps;
     gate.or_weight2 = (cost(model) - c) / eps;
-    model.or_weight2 = saved;
+    model.or_weight2 = stored_value;
 
-    saved = model.or_bias;
+    stored_value = model.or_bias;
     model.or_bias += eps;
     gate.or_bias = (cost(model) - c) / eps;
-    model.or_bias = saved;
+    model.or_bias = stored_value;
 
-    saved = model.nand_weight1;
+    stored_value = model.nand_weight1;
     model.nand_weight1 += eps;
     gate.nand_weight1 = (cost(model) - c) / eps;
-    model.nand_weight1 = saved;
+    model.nand_weight1 = stored_value;
 
-    saved = model.nand_weight2;
+    stored_value = model.nand_weight2;
     model.nand_weight2 += eps;
     gate.nand_weight2 = (cost(model) - c) / eps;
-    model.nand_weight2 = saved;
+    model.nand_weight2 = stored_value;
 
-    saved = model.nand_bias;
+    stored_value = model.nand_bias;
     model.nand_bias += eps;
     gate.nand_bias = (cost(model) - c) / eps;
-    model.nand_bias = saved;
+    model.nand_bias = stored_value;
 
-    saved = model.and_weight1;
+    stored_value = model.and_weight1;
     model.and_weight1 +=eps;
     gate.and_weight1 = (cost(model) - c) / eps;
-    model.and_weight1 = saved;
+    model.and_weight1 = stored_value;
 
-    saved = model.and_weight2;
+    stored_value = model.and_weight2;
     model.and_weight2 +=eps;
     gate.and_weight2 = (cost(model) - c) / eps;
-    model.and_weight2 = saved;
+    model.and_weight2 = stored_value;
 
-    saved = model.and_bias;
+    stored_value = model.and_bias;
     model.and_bias += eps;
     gate.and_bias = (cost(model) - c) / eps;
-    model.and_bias = saved;
+    model.and_bias = stored_value;
 
     return gate;
 }
 
 int main(void)
 {
+    srand(time(0));
     Xor model = rand_xor();
 
     float eps = 1e-1;
     float rate = 1e-1;
 
-    for (size_t i = 0; i < 10 * 1000; ++i) {
+    // Print cost
+    for (size_t i = 0; i < 1 * 1000; ++i) {
         Xor gate = finite_difference(model, eps);
         model = learn(model, gate, rate);
-        printf("COST: %f\n", cost(model));
+        //printf("COST: %f\n", cost(model));
     }
+
+    printf("---------------------\n");
+    for (size_t i = 0; i < 2; ++i) {
+        for (size_t j = 0; j < 2; ++j) {
+            printf("%zu ^ %zu = %f\n", i, j, forward(model, i, j));
+        }
+    }
+
+    printf("---------------------\n");
+    printf("\"OR\" neuron:\n");
+    for (size_t i = 0; i < 2; ++i) {
+        for (size_t j = 0; j < 2; ++j) {
+           printf("%zu or %zu = %f\n", i, j, sigmoidf(model.or_weight1*i + model.or_weight2 * j+model.or_bias));
+        }
+    }
+
+    printf("---------------------\n");
+    printf("\"NAND\" neuron:\n");
+    for (size_t i = 0; i < 2; ++i) {
+        for (size_t j = 0; j < 2; ++j) {
+           printf("~(%zu and %zu) = %f\n", i, j, sigmoidf(model.nand_weight1*i + model.nand_weight2 * j+model.nand_bias));
+        }
+    }
+
+    printf("---------------------\n");
+    printf("\"AND\" neuron:\n");
+    for (size_t i = 0; i < 2; ++i) {
+        for (size_t j = 0; j < 2; ++j) {
+           printf("%zu and %zu = %f\n", i, j, sigmoidf(model.and_weight1*i + model.and_weight2 * j+model.and_bias));
+        }
+    }
+    return 0;
 }
